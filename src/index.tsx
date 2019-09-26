38d05b2b81626f1cf3547351dfbf4388aebe3438
shuta13 <hollystarsun@gmail.com>
2019-09-26 11:23:50 +0900

    :recycle: とりあえずreactの流儀までやったのでpush

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,64 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+interface SignUpDialog {
+  state: {
+    login: string
+  }
+}
+
+function FancyBorder(props: any) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+  )
+}
+
+function Dialog(props: any) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        {props.title}
+      </h1>
+      <p className="Dialog-message">
+        {props.message}
+      </p>
+      {props.children}
+    </FancyBorder>
+  );
+}
+
+class SignUpDialog extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {login: ''};
+  }
+
+  render() {
+    return (
+      <Dialog title="Mars Exploration Program"
+              message="How should we refer to you?">
+        <input value={this.state.login} onChange={this.handleChange} />
+        <button onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </Dialog>
+    );
+  }
+
+  handleChange(e: any) {
+    this.setState({login: e.target.value});
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}`)
+  }
+}
+
 ReactDOM.render(
-  <div> 贅沢な名前だねぇ </div>,
+  <SignUpDialog />,
   document.getElementById('root')
 );
